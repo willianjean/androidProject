5acdb0af30b11de7f32d64a725e4665226908127
willianjean <willianjean@c42bce11-e1ae-487f-873a-6def430b28f2>
2014-07-15 17:20:03 +0000

    update

--- trunk/Aula05/gen/aula/android/aula05/R.java
+++ trunk/Aula05/gen/aula/android/aula05/R.java
@@ -1068,10 +1068,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int aparecida=0x7f020057;
+        public static final int cachoeirinha=0x7f020058;
+        public static final int centro=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int praca14=0x7f02005b;
     }
     public static final class id {
-        public static final int Principal=0x7f05003c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1084,6 +1087,7 @@ containing a value of this type.
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int ajuda=0x7f050042;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btnCadastro=0x7f050040;
@@ -1095,17 +1099,19 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int edtNome=0x7f05003e;
-        public static final int edtRegiao=0x7f05003f;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int historico=0x7f050043;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imgBairro=0x7f050041;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int llPrincipal=0x7f05003c;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1127,6 +1133,7 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spRegiao=0x7f05003f;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1190,6 +1197,9 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int activity_mensagem=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+    }
+    public static final class menu {
+        public static final int context_menu=0x7f0c0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
