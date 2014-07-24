8deaf584229c00904efab414924d3c175f61a248
willianjean <willianjean@c42bce11-e1ae-487f-873a-6def430b28f2>
2014-07-24 18:32:49 +0000

    update

--- Exercicio3/src/aula/android/exercicio3/MainActivity.java
+++ Exercicio3/src/aula/android/exercicio3/MainActivity.java
@@ -9,16 +9,18 @@ import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 	
 	private static final int DATE_DIALOG_ID = 0;
 	private int ano, mes, dia;
 	private EditText nome;
-	private Button dataNas;
+	private Button btnDataNas;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +33,13 @@ public class MainActivity extends Activity {
 		mes = calendar.get(Calendar.MONTH);
 		dia = calendar.get(Calendar.DAY_OF_MONTH);
 		
-		dataNas = (Button)findViewById(R.id.data);
+		btnDataNas = (Button)findViewById(R.id.data);
+		btnDataNas.setText(dia + "/" + (mes+1) + "/" + ano);
 		
-		dataNas.setOnClickListener(new View.OnClickListener() {
+		nome = (EditText)findViewById(R.id.nome);
+		
+		//botão para setar a data de nascimento
+		btnDataNas.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -42,18 +48,44 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+		//botao da habilitação
+		Button btnHabilitacao = (Button)findViewById(R.id.enviarHabilitacao);
 		
-		Button btn = (Button)findViewById(R.id.enviar);
-		nome = (EditText) findViewById(R.id.nome);
-		btn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				mostraJanela();
-			}
-		});
+		btnHabilitacao.setOnClickListener(this);
+		//A chamada abaixo foi substituido pela linha acima e tem o mesmo efeito
 		
+		//botao de Tirar Habilitação
+//		btnHabilitacao.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				if(nome.length()==0){
+//					Toast.makeText(MainActivity.this, "Por favor informe seu nome", Toast.LENGTH_SHORT).show();
+//					return;}
+//				else mostraJanelaHabilitacao();
+//			}
+//		});
+		
+		//botao de habilitação
+		Button btnAposentadoria = (Button)findViewById(R.id.enviarAposentadoria);
+		
+		btnAposentadoria.setOnClickListener(this);
+		//A chamada abaixo foi substituido pela linha acima e tem o mesmo efeito
+		
+		//botao de Aposentar
+//		btnAposentadoria.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				if(nome.length()==0){
+//					Toast.makeText(MainActivity.this, "Por favor informe seu nome", Toast.LENGTH_SHORT).show();
+//					return;}
+//				else mostraJanelaAposentadoria();
+//			}
+//			
+//		});
 		
 	}
 	
@@ -64,11 +96,12 @@ public class MainActivity extends Activity {
 		switch (id){
 		case DATE_DIALOG_ID:
 			DatePickerDialog dp = new DatePickerDialog(this, listener, ano, mes, dia);
-		
+			return dp;
 		}		
 		return null;
 	}
 	
+	//classe anonima para passar como parametro em Dialog onCreateDialog
 	private OnDateSetListener listener = new OnDateSetListener() {
 		
 		@Override
@@ -79,18 +112,49 @@ public class MainActivity extends Activity {
 			mes = monthOfYear;
 			dia = dayOfMonth;
 			
-			dataNas.setText(dia + "/" + mes + "/" + ano);
+			btnDataNas.setText(dia + "/" + mes + "/" + ano);
 		}
 	};
 	
-	public void mostraJanela(){
+	public void mostraJanelaHabilitacao(){
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Quanto falta para tirar a carteira");
-		builder.setMessage(nome.getText().toString());
-		AlertDialog alerta = builder.create();
-		
-		//time
-		
+		builder.setTitle("Quanto falta para tirar a carteira de habilitação?");
+		Calendar calendar = Calendar.getInstance();
+		if((calendar.get(Calendar.YEAR)-ano)>=18)
+			builder.setMessage("Você ja pode tirar sua habilitação");
+		else builder.setMessage(nome.getText()+", falta(m) "+String.valueOf((18-(calendar.get(Calendar.YEAR)-ano)))+" ano(s)");
+		AlertDialog alerta = builder.create();		
 		alerta.show();
+	}
+	
+	public void mostraJanelaAposentadoria(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Quanto falta para se aposentar?");
+		Calendar calendar = Calendar.getInstance();
+		if((calendar.get(Calendar.YEAR)-ano)>=65)
+			builder.setMessage("Você ja pode se aposentar");
+		else builder.setMessage(nome.getText()+", falta(m) "+String.valueOf((65-(calendar.get(Calendar.YEAR)-ano)))+" ano(s)");
+		AlertDialog alerta = builder.create();		
+		alerta.show();
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.enviarHabilitacao:
+			if(nome.length()==0){
+				Toast.makeText(this, "Por favor informe seu nome", Toast.LENGTH_SHORT).show();
+				break;}
+			else {mostraJanelaHabilitacao();
+			break;}
+		case R.id.enviarAposentadoria:
+			if(nome.length()==0){
+				Toast.makeText(this, "Por favor informe seu nome", Toast.LENGTH_SHORT).show();
+				break;}
+			else {mostraJanelaAposentadoria();
+			break;}
+		}
+			
 	}
 }
