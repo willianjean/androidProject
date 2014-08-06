0c1e8613f2727bb762c70f200d28e2c2dfb7cd6b
willianjean <willianjean@c42bce11-e1ae-487f-873a-6def430b28f2>
2014-08-06 22:17:22 +0000

    new files included to solve Taylor series

---
+++ teste/src/br/ufam/CalculadoraTestTest.java
@@ -0,0 +1,14 @@
+package br.ufam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
